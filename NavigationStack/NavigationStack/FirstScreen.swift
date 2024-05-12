f2dab359fa4b1e93782e0ceed8d6af9310d74909
Minyoung Lee <mminy62@gmail.com>
2024-05-13 00:10:44 +0900

    [Feat] 첫 시작 페이지 코드 베이스로 생성

--- NavigationStack/NavigationStack/FirstScreen.swift
+++ NavigationStack/NavigationStack/FirstScreen.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class FirstScreen: UIViewController {
+    
+    let nextButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .systemMint
+        setupButton()
+        view.backgroundColor = .systemBackground
+        title = "First Screen"
     }
-
+    
+    func setupButton() {
+        view.addSubview(nextButton)
+        // configuration: determined how it looks
+        nextButton.configuration = .filled()
+        nextButton.configuration?.baseBackgroundColor = .systemPink
+        nextButton.configuration?.title = "Next"
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        // standard => view centerX
+//        nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        
+    }
 
 }
 
