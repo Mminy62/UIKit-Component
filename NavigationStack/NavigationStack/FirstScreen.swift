78e295f954bab0ad1be94d12b84a864fc67cf77f
Minyoung Lee <mminy62@gmail.com>
2024-05-12 23:34:32 +0900

    🎉 [Init] Storyboard 제거 및 프로젝트 초기 세팅

---
+++ NavigationStack/NavigationStack/FirstScreen.swift
@@ -0,0 +1,20 @@
+//
+//  FirstScreen.swift
+//  NavigationStack
+//
+//  Created by 이민영 on 5/12/24.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
