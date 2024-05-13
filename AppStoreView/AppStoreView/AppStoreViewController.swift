31d730bdb6ddf30d69099860f28f95e7a8aced65
Minyoung Lee <mminy62@gmail.com>
2024-05-13 15:13:27 +0900

    [Feat] home에 collection view 추가

--- AppStoreView/AppStoreView/AppStoreViewController.swift
+++ AppStoreView/AppStoreView/AppStoreViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class AppStoreViewController: UIViewController {
     
-    // uiview controller 안에 collection view 넣기 
+    // uiview controller 안에 collection view 넣기
     private lazy var collectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout() // CollectionView layout object
@@ -23,9 +23,46 @@ class AppStoreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "App Store"
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupCollectionView()
+        
     }
-
+    
+    func setupCollectionView() {
+        view.addSubview(collectionView)
+        // 화면 꽉 차게 세팅
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        collectionView.dataSource = self
+        collectionView.register(FeaturedAppsCell.self, forCellWithReuseIdentifier: FeaturedAppsCell.identifier)
+        
+    }
 
 }
 
+extension AppStoreViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        featuredApps.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // indexPath: 셀의 위치를 특정
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeaturedAppsCell.identifier, for: indexPath) as! FeaturedAppsCell
+        cell.featuredApppItem = featuredApps[indexPath.row]
+        
+        return cell
+    }
+}
---
+++ AppStoreView/AppStoreView/Cells/FeaturedAppsCell.swift
@@ -0,0 +1,124 @@
+//
+//  FeaturedAppsCell.swift
+//  AppStoreView
+//
+//  Created by 이민영 on 5/13/24.
+//
+
+import UIKit
+import Kingfisher
+import Nuke
+import NukeUI
+import NukeExtensions
+// NUKE Github : https://github.com/kean/Nuke
+
+class FeaturedAppsCell: UICollectionViewCell {
+    
+    static let identifier = "featuredAppsCell"
+    
+    var featuredApppItem: FeaturedApp! {
+        didSet {
+            bindCell(with: featuredApppItem)
+        }
+    }
+    
+    private lazy var titleLabel: UILabel =  {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var appNameLabel: UILabel =  {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 20)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var appIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 10
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var vStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureCell() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(vStack)
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            vStack.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
+            vStack.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8),
+            vStack.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant:  8),
+            vStack.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8)
+        ])
+        [titleLabel, appNameLabel, appIconImageView].forEach { vStack.addArrangedSubview($0) }
+    }
+    
+    private func bindCell(with item: FeaturedApp) {
+        titleLabel.text = item.title
+        appNameLabel.text = item.appName
+        appIconImageView.kf.setImage(with: item.iconURL)
+    }
+    
+
+    
+}
+
+
+
+#if DEBUG
+import SwiftUI
+import NukeExtensions
+struct FeaturedAppsCollectionView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    func makeUIView(context: Context) -> UIView {
+        let cell = FeaturedAppsCell()
+        cell.featuredApppItem = featuredApps.first!
+        return cell
+    }
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+struct FeaturedAppsCollectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedAppsCollectionView()
+            .edgesIgnoringSafeArea(.all)
+            .previewLayout(.fixed(width: 300, height: 200))
+    }
+}
+#endif
