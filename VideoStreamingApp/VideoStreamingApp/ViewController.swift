de7dd8e4f5d2649bb4140db8c6da94a84df40b3b
Minyoung Lee <mminy62@gmail.com>
2024-05-22 23:55:07 +0900

    [Init] K사 video streaming project 생성

---
+++ VideoStreamingApp/VideoStreamingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStreamingApp
+//
+//  Created by 이민영 on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
