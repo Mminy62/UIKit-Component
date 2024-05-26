a1f053751f0a4283ff95ea88404291a553e3ed77
Minyoung Lee <mminy62@gmail.com>
2024-05-26 23:50:17 +0900

    Init: CollectionView 프로젝트 생성

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by 이민영 on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
