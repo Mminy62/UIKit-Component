b3eb8e24fe5e4c4ef7207fa7a20e6e4f88da168e
Minyoung Lee <mminy62@gmail.com>
2024-05-28 00:12:15 +0900

    [Design] HomeViewController에 table view 추가

---
+++ VideoStreamingApp/VideoStreamingApp/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  VideoStreamingApp
+//
+//  Created by 이민영 on 5/27/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
