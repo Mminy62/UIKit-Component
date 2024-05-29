f016a08b020c90c32e54c288bb488b93143caa8b
Minyoung Lee <mminy62@gmail.com>
2024-05-29 17:02:18 +0900

    [Design] HomeVideoCellView xib 구현

---
+++ VideoStreamingApp/VideoStreamingApp/Cell/HomeVideoCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeVideoCell.swift
+//  VideoStreamingApp
+//
+//  Created by 이민영 on 5/28/24.
+//
+
+import UIKit
+
+class HomeVideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
