0ae3ea6667747daaef750703cd31224fb9b713f4
Minyoung Lee <mminy62@gmail.com>
2024-06-04 11:31:09 +0900

    Init: FirstApp 생성 및 UI 설정

---
+++ FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 이민영 on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
