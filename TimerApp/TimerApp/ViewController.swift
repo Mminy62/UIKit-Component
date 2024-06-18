471d1310ebcb2414a09028a6d0ccbccb12d86037
Minyoung Lee <mminy62@gmail.com>
2024-06-19 00:35:39 +0900

    Init: Timer 프로젝트 생성

---
+++ TimerApp/TimerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by 이민영 on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
