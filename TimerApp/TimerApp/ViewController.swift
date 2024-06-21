576076a1d82c90cfd83373675652f82b93770fb0
Minyoung Lee <mminy62@gmail.com>
2024-06-22 01:58:32 +0900

    Feat: Timer 함수 정의

--- TimerApp/TimerApp/ViewController.swift
+++ TimerApp/TimerApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mainLabel: UILabel!
     
+    @IBOutlet weak var slider: UISlider!
     
     
     override func viewDidLoad() {
@@ -22,19 +23,39 @@ class ViewController: UIViewController {
     
     func configureUI() {
         mainLabel.text = "초를 선택하세요"
+        // 슬라이더의 가운데 설정
+//        slider.minimumValue = 0.0
+//        slider.maximumValue = 60.0
+//        slider.value = 30.0
+        
     }
 
 
     @IBAction func sliderChanged(_ sender: UISlider) {
-        
+        // 슬라이더의 벨류 값을 가지고 메인 레이블에 텍스트 세팅
+        let seconds = Int(slider.value * 60)
+        mainLabel.text = "\(seconds) 초"
     }
     
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
+        // 1초씩 지나갈때마다 무언가를 실행
+//        while slider.value * 60 > 0 {
+//            let seconds = slider.value * 60
+//            slider.value = (seconds - 1) / 60
+//            
+//            print(slider.value)
+//            slider.
+//        }
+
+
     }
     
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        mainLabel.text = "초를 선택하세요"
+        slider.setValue(0.5, animated: true)
+        
     }
 }
 
