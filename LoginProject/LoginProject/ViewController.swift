0bf8d875f4c2432e392c17fdf967d337e0665d5e
Minyoung Lee <mminy62@gmail.com>
2024-08-28 18:22:51 +0900

    Feat: 필요한 로그인 뷰 선언

--- LoginProject/LoginProject/ViewController.swift
+++ LoginProject/LoginProject/ViewController.swift
@@ -8,6 +8,113 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: 이메일 입력하는 텍스트 뷰
+    private lazy var emailTextFieldView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+        view.layer.cornerRadius = 5
+        view.clipsToBounds = true
+//        view.addSubview(emailTextFieldView)
+        
+        return view
+    }()
+    
+    
+    // MARK: "이메일 또는 전화번호" 안내 문구
+    private var emailInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이메일주소 또는 전화번호"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        
+        return label
+    }()
+    
+    // MARK: 로그인 - 이메일 입력 필드
+    private lazy var emailTextField: UITextField = {
+        var tf = UITextField()
+        tf.frame.size.height = 48
+        tf.backgroundColor = .clear
+        tf.textColor = .white
+        tf.tintColor = .white
+        tf.autocapitalizationType = .none // 자동 대문자 수정
+        tf.autocorrectionType = .no // 자동 수정
+        tf.spellCheckingType = .no // 스펠링 체크
+        tf.keyboardType = .emailAddress
+        
+        return tf
+    }()
+    
+    // MARK: 비밀번호 입력하는 텍스트 뷰
+    private lazy var passwordTextFieldView: UIView = {
+        let view = UIView()
+        view.frame.size.height = 48
+        view.backgroundColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+        view.layer.cornerRadius = 5
+        view.clipsToBounds = true
+
+        return view
+    }()
+    
+    // MARK: 비밀번호 - 비밀번호 안내 문구
+    private var passwordInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        
+        return label
+    }()
+    
+    // MARK: 비밀번호 - 비밀번호 입력 필드
+    private lazy var passwordTextField: UITextField = {
+        var tf = UITextField()
+        tf.frame.size.height = 48
+        tf.backgroundColor = .clear
+        tf.textColor = .white
+        tf.tintColor = .white
+        tf.autocapitalizationType = .none // 자동 대문자 수정
+        tf.autocorrectionType = .no // 자동 수정
+        tf.spellCheckingType = .no // 스펠링 체크
+        tf.isSecureTextEntry = true
+        tf.clearsOnBeginEditing = false
+        
+        return tf
+    }()
+    
+    // MARK: "표시"버튼 비밀번호 가리기 기능
+    private let passwordSecureButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("표시", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        return button
+    }()
+    
+    // MARK: 로그인 버튼
+    private var loginButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 5
+        button.clipsToBounds = true
+        button.layer.borderWidth = 1
+        button.layer.borderColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+        button.setTitle("로그인", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.isEnabled = false
+        
+        return button
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [emailTextFieldView,
+                                                 passwordTextFieldView, loginButton])
+        view.spacing = 18
+        view.axis = .vertical
+        view.distribution = .fillEqually
+        view.alignment = .fill
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +123,7 @@ class ViewController: UIViewController {
     }
     
     func configureUI() {
-      
+        
     }
 
 
