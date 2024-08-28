16f6819e07be87e3b6734ecd842151e37af87ddb
Minyoung Lee <mminy62@gmail.com>
2024-08-28 17:28:44 +0900

    Delete: test code 삭제

--- LoginProject/LoginProject/ViewController.swift
+++ LoginProject/LoginProject/ViewController.swift
@@ -9,14 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let emailTextField: UIView = {
-        let view = UIView() // 메모리에 올라가는 상태
-        view.backgroundColor = UIColor.darkGray
-        view.layer.cornerRadius = 10
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view // view를 emailTextField에 담는 것
-    }() // 클로저 실행문 타입
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -24,11 +16,7 @@ class ViewController: UIViewController {
     }
     
     func configureUI() {
-        view.addSubview(emailTextField) // 코드로 하려면 addSubview 함수로 올려주는 역할이 필요하다.
-        emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        emailTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
-        emailTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+      
     }
 
 
