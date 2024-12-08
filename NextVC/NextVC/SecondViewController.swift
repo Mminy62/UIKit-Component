873dc88a6cc27ec3d592230c949824d00a949cbc
Minyoung Lee <mminy62@gmail.com>
2024-12-08 20:15:23 +0900

    Feat: 화면 전환 방법별 코드 구현

--- NextVC/NextVC/SecondViewController.swift
+++ NextVC/NextVC/SecondViewController.swift
@@ -11,16 +11,18 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var mainLabel: UILabel!
     
+    var someString: String?
     
+    // 스토리보드가 메모리에 올라가고 -> 코드 올라가고 -> 둘이 연결된 후 가장 먼저 불리는 함수가 viewDidLoad
+    // 그러므로, 아직 연결이 되지 않은 상태에서 viewController에서 mainLabel에 접근해서 문제가 되는 것임
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        mainLabel.text = someString
     }
     
     
     @IBAction func backButtonTapped(_ sender: UIButton) {
-        
+        dismiss(animated: true)
     }
     
 }
--- NextVC/NextVC/ViewController.swift
+++ NextVC/NextVC/ViewController.swift
@@ -19,35 +19,60 @@ class ViewController: UIViewController {
     @IBAction func codeNextButtonTapped(_ sender: UIButton) {
         let firstVC = FirstViewController()
         firstVC.someString = "안녕?!"
-        // firstVC.mainLabel.text = "안녕" // 이렇게 직접적으로 전달하진 않음 => 스토리보드로 만들면 에러나는 코드
-        // 코드는 한번에 메모리에 올라감
-        // 코드 -> 스토리보드 -> 둘 연결 이런 식으로 메모리에 올라가는 순서이기 때문에, mainLabel.text에 접근할 수 없음(스토리보드의 ui가 없기 때문에 mainLabel에 접근이 안됨)
-        // 보통 데이터를 전달할 땐, 클래스/구조체를 전달하는 방법이 많음
         firstVC.modalPresentationStyle = .fullScreen
-        present(firstVC, animated: true, completion: nil) // present 라는 함수로 firstVC로 던져준다
-        // 기본적으로는 modal 스타일로 view를 보여준다.
-        
-        
+        present(firstVC, animated: true, completion: nil) // present 라는 함수로 firstVC로 던져준다, 기본적으로 modal 타입
     }
     
     // 2) 코드로 스토리보드 객체를 생성해서, 화면 이동
     @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
-
+        // 메모리에 secondViewController가 힙 영역에 이미 있음(storyboard)
+        // UIViewController 안에 storyboard가 선언되어 있음
+        // storyboard에 있는 식별자를 기준으로 연결해줘야함
         
-        
-        
-        
+        // 타입캐스팅이 필요
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController
+        secondVC.someString = "아빠상어"
+        present(secondVC, animated: true, completion: nil)
     }
     
     
     
     // 3) 스토리보드에서의 화면 이동(간접 세그웨이)
     @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
+        // 버튼 눌릴 때, 실행
+        performSegue(withIdentifier: "toThirdVC", sender: self) // sender: 보내는 화면
+    }
+    
+    // 세그웨이로 데이터 전달
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 1. id 확인
+        // 2. destination에 접근해서 타입 캐스팅
+        // 3. 데이터 전달
+        if segue.identifier == "toThirdVC" {
+            let thirdVC = segue.destination as! ThirdViewController
+            thirdVC.someString = "엄마상어"
+            
+        }
         
-
-        
-        
-        
+        if segue.identifier == "toFourthVC" {
+            let fourthVC = segue.destination as! FourthViewController
+            fourthVC.someString = "네번째화면"
+            
+        }
+    }
+    
+    // 4) 직접 세그웨이
+    // 버튼에 직접적으로 seuge를 연결했을때만 실행 가능
+    // 버튼에 직접 segue를 연결, 내부적으론 performSegue로 실행
+    // 조건문에 따라 실행하도록, shouldPerfomSegue 함수 실행
+    var num = 7
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if num > 5 {
+            return false
+        } else {
+            return true // 다음화면으로 넘어가는 것
+        }
         
     }
     
