45ae93c503751ebc45dfa58d49031d4a5ccf4e47
Minyoung Lee <mminy62@gmail.com>
2025-01-11 18:29:10 +0900

    Init: Chatting UI project 생성

---
+++ ChattingProject/ChattingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChattingProject
+//
+//  Created by 이민영 on 1/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
