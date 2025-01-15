4b31d492b5874149d1aecd8461f3ec6b2d050318
Minyoung Lee <mminy62@gmail.com>
2025-01-15 15:43:02 +0900

    Init: NaverShopping 프로젝트 생성

---
+++ NaverShopping/NaverShopping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaverShopping
+//
+//  Created by 이민영 on 1/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
