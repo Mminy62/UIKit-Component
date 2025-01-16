8634b7fd08a0f8f41c7208e31660adb0bed0e544
Minyoung Lee <mminy62@gmail.com>
2025-01-16 12:28:49 +0900

    Refactor: BaseTableView 상속으로 UI 코드 수정

--- FourthWeek/FourthWeek/Market/MarketTableViewCell.swift
+++ FourthWeek/FourthWeek/Market/MarketTableViewCell.swift
@@ -8,40 +8,34 @@
 import UIKit
 import SnapKit
 
-class MarketTableViewCell: UITableViewCell, NSCoding {
+class MarketTableViewCell: BaseTableViewCell {
     
     static let id = "MarketTableViewCell"
     
 //    let nameLabel = UILabel()
     let nameLabel = BlackRadiusLabel() // 매개변수 없이 호출해도, 알아서 init(frame:)이 호출되도록 설정됨
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        configureHierarchy()
-        configureLayout()
-        configureView()
-        
-    }
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        
+//        configureHierarchy()
+//        configureLayout()
+//        configureView()
+//        
+//    }
     
-    func configureHierarchy() {
+    override func configureHierarchy() {
         contentView.addSubview(nameLabel)
     }
     
-    func configureLayout() {
+    override func configureLayout() {
         nameLabel.snp.makeConstraints { make in
             make.edges.equalTo(contentView.safeAreaLayoutGuide).inset(16)
         }
     }
     
-    func configureView() {
+    override func configureView() {
 //        nameLabel.textColor = .brown
 //        nameLabel.font = .boldSystemFont(ofSize: 20)
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
 }
